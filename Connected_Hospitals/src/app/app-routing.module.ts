2776fe3110c290f87a0778cb00d8d442ae7c7661
rdwebushi <rushikesh26deshpande@gmail.com>
2022-09-15 11:31:22 +0530

    more component

--- Connected_Hospitals/src/app/app-routing.module.ts
+++ Connected_Hospitals/src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { EmergencyComponent } from './emergency/emergency.component';
+import { PatientComponent } from './patient/patient.component';
+import { RegistrationComponent} from './registration/registration.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'/login',
+    component: LoginComponent
+  },
+  {
+    path:'/emergency',
+    component: EmergencyComponent
+  },
+  {
+    path:'/patient',
+    component: PatientComponent
+  },
+  {
+    path:'/registration',
+    component: RegistrationComponent
+  },
+  {
+    path:'/aboutus',
+    component: AboutUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
