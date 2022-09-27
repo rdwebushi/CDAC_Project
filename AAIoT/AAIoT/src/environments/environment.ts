6c11b69a4797ac4d8f15c2a95e45c27c2a0caf54
rdwebushi <rushikesh26deshpande@gmail.com>
2022-09-27 15:11:20 +0530

    data send to atlas mongodb

--- AAIoT/AAIoT/src/environments/environment.ts
+++ AAIoT/AAIoT/src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   apiBaseUrl:'http://localhost:3000/api'
-
-};
+  };
 
 /*
  * For easier debugging in development mode, you can import the following file
